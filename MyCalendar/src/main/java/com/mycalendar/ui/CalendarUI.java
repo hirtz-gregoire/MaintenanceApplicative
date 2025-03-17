b1d3bfcb5378b8e44d8f919000c39abc0a958ec0
hirtz-gregoire <hirtz.gregoire@ghmail.fr>
2025-03-17 09:45:35 +0100

    🎨add(CalendarUI): changement ASCII art

--- MyCalendar/src/main/java/com/mycalendar/ui/CalendarUI.java
+++ MyCalendar/src/main/java/com/mycalendar/ui/CalendarUI.java
@@ -22,15 +22,11 @@ public class CalendarUI {
     }
     
     public void displayLogo() {
-        System.out.println("  _____         _                   _                __  __");
-        System.out.println(" / ____|       | |                 | |              |  \\/  |");
-        System.out.println("| |       __ _ | |  ___  _ __    __| |  __ _  _ __  | \\  / |  __ _  _ __    __ _   __ _   ___  _ __");
-        System.out.println("| |      / _` || | / _ \\| '_ \\  / _` | / _` || '__| | |\\/| | / _` || '_ \\  / _` | / _` | / _ \\| '__|");
-        System.out.println("| |____ | (_| || ||  __/| | | || (_| || (_| || |    | |  | || (_| || | | || (_| || (_| ||  __/| |");
-        System.out.println(" \\_____| \\__,_||_| \\___||_| |_| \\__,_| \\__,_||_|    |_|  |_| \\__,_||_| |_| \\__,_| \\__, | \\___||_|");
-        System.out.println("                                                                                   __/ |");
-        System.out.println("                                                                                  |___/");
+        System.out.println("░▒█▀▀▄░█▀▀▄░█░░█▀▀░█▀▀▄░█▀▄░█▀▀▄░█▀▀▄░▒█▀▄▀█░█▀▀▄░█▀▀▄░█▀▀▄░█▀▀▀░█▀▀░█▀▀▄");
+        System.out.println("░▒█░░░░█▄▄█░█░░█▀▀░█░▒█░█░█░█▄▄█░█▄▄▀░▒█▒█▒█░█▄▄█░█░▒█░█▄▄█░█░▀▄░█▀▀░█▄▄▀");
+        System.out.println("░▒█▄▄▀░▀░░▀░▀▀░▀▀▀░▀░░▀░▀▀░░▀░░▀░▀░▀▀░▒█░░▒█░▀░░▀░▀░░▀░▀░░▀░▀▀▀▀░▀▀▀░▀░▀▀");
     }
+    
     
     public boolean displayMainMenu(User user) {
         System.out.println("\nBonjour, " + user.getUsername());
